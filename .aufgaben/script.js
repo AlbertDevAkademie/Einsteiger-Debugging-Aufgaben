cf74980f0b08d88b6a2700fe763c7b27ff1d9328
albert-wissigkeit <wissigkeit@outlook.de>
2025-01-07 16:15:29 +0100

    first commit

---
+++ .aufgaben/script.js
@@ -0,0 +1,27 @@
+// Aufgabe 1
+function sayHello() {
+    alert("Hallo, Welt!");
+}
+
+// Aufgabe 2
+function showName(name) {
+    console.log("Der Name ist: " + name.toUpperCase());
+}
+
+// Aufgabe 3
+function multiplyNumbers(a, b) {
+    let result = "b" * "a"
+    console.log(result);
+}
+
+// Aufgabe 4
+function changeColor() {
+    document.getElementById('examplle4').style.backgroundColor = "green";
+}
+
+// Aufgabe 5
+function greetPerson(person) {
+    let inputName = document.getElementById('example5').value;
+    let text = "Hallo " + inputName + ", du bist " + person.name + " Jahre alt!"
+    document.getElementById('example5').innerHTML = text;
+}
